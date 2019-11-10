a1f5def1af60aa471ac71e605b8a7aff2f2ba87f
Lilkhachatryan <lilit-messi@mail.ru>
2019-11-11 02:06:33 +0400

    init

---
+++ build/precache-manifest.fab8bd088bbf32b2eacac1e683d0c89f.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2fad0ed783e308bd5b3f18285bcf8e01",
+    "url": "/index.html"
+  },
+  {
+    "revision": "e6ef81d20e510fab7904",
+    "url": "/static/css/main.c62c1a04.chunk.css"
+  },
+  {
+    "revision": "4563fc0dcacddd1fe00c",
+    "url": "/static/js/2.a0daa9da.chunk.js"
+  },
+  {
+    "revision": "e6ef81d20e510fab7904",
+    "url": "/static/js/main.386d8b80.chunk.js"
+  },
+  {
+    "revision": "b82dff8c4324a14baa2a",
+    "url": "/static/js/runtime-main.8f3aebbd.js"
+  },
+  {
+    "revision": "25bf045ca257e971124f3997d89f321c",
+    "url": "/static/media/logo.25bf045c.svg"
+  }
+]);
