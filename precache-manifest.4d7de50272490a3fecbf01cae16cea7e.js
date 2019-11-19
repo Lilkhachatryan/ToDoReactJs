594a23273192402e8f96d9e99c2b6c5f6a1b8397
Lilkhachatryan <lilit-messi@mail.ru>
2019-11-20 00:08:33 +0400

    new build

---
+++ precache-manifest.4d7de50272490a3fecbf01cae16cea7e.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5d2b696fd74d5fa94b36c67519f2c7cd",
+    "url": "/ToDoReactJs/index.html"
+  },
+  {
+    "revision": "874a4d6500760ae7ee4b",
+    "url": "/ToDoReactJs/static/css/main.9ab31add.chunk.css"
+  },
+  {
+    "revision": "4563fc0dcacddd1fe00c",
+    "url": "/ToDoReactJs/static/js/2.a0daa9da.chunk.js"
+  },
+  {
+    "revision": "874a4d6500760ae7ee4b",
+    "url": "/ToDoReactJs/static/js/main.50216f97.chunk.js"
+  },
+  {
+    "revision": "c712e0f92ff17494acb3",
+    "url": "/ToDoReactJs/static/js/runtime-main.9f7c705d.js"
+  },
+  {
+    "revision": "25bf045ca257e971124f3997d89f321c",
+    "url": "/ToDoReactJs/static/media/logo.25bf045c.svg"
+  }
+]);
