6fd866585ad8ae49cf927ad434b964eda05c99d8
unknown <Lilkhachatyan>
2019-11-29 23:07:36 -0800

    Updates

---
+++ precache-manifest.0fa72229b55da466d048e9e253bc4072.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6e3680c59ca2813bbcd87562b8ad55a8",
+    "url": "/ToDoReactJs/index.html"
+  },
+  {
+    "revision": "7f3fea41221a7b1f61b3",
+    "url": "/ToDoReactJs/static/css/main.9ab31add.chunk.css"
+  },
+  {
+    "revision": "e3ddc8e01d9de1110fc4",
+    "url": "/ToDoReactJs/static/js/2.54c6d3d1.chunk.js"
+  },
+  {
+    "revision": "7f3fea41221a7b1f61b3",
+    "url": "/ToDoReactJs/static/js/main.cd28664a.chunk.js"
+  },
+  {
+    "revision": "c712e0f92ff17494acb3",
+    "url": "/ToDoReactJs/static/js/runtime-main.9f7c705d.js"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.90753af1084bb0f99a1edfc0f3e0e6da.js"
+  "/ToDoReactJs/precache-manifest.0fa72229b55da466d048e9e253bc4072.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/ToDoReactJs/index.html"), {
   
-  blacklist: [/^\/_/,/\/[^\/?]+\.[^\/]+$/],
+  blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
